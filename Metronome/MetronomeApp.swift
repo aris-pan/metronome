1df2af00bd9a0856ee52068b9b6e553acacbb4de
aris-pan <arispanagiwtopoulos@gmail.com>
2023-05-19 15:20:33 +0300

    Initial Commit

---
+++ Metronome/MetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetronomeApp.swift
+//  Metronome
+//
+//  Created by Aris on 19/5/23.
+//
+
+import SwiftUI
+
+@main
+struct MetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
